93d41f60337aceb2e615d32a4646b431d3a8d9bd
Juandy Ocampo <corajejd@gmail.com>
2018-10-26 23:58:26 -0300

    [ADD] 3 testeos más en DownTest.java

--- src/treesetsimple/test/DownTest.java
+++ src/treesetsimple/test/DownTest.java
@@ -8,16 +8,16 @@ public class DownTest extends Test
     }
     //<editor-fold desc="relleno de datos">
         private Object[] restart(){
-            return new Object[]{ "Deborah","Tommy","Franco","Manuela","Miguel","Denisse" };
+            return new Object[]{"Deborah","Tommy","Franco","Manuela","Miguel","Denisse"};
         }
         private Object[] elem_1(){
-            return new Object[]{ "Deborah" };        
+            return new Object[]{"Deborah"};        
         }
         private Object[] elem_2(){
-            return new Object[]{ "Deborah","Franco" };
+            return new Object[]{"Deborah","Franco"};
         }
         private Object[] elem_3(){
-            return new Object[]{ "Deborah","Franco","Tommy" };
+            return new Object[]{"Deborah","Franco","Tommy"};
         }
     //</editor-fold>
     //<editor-fold desc="pruebas">
@@ -25,7 +25,7 @@ public class DownTest extends Test
             set.clear();
             for (Object obj : arreglo) {
                 set.add(obj.toString());
-            }
+            }            
             for(Object c : criterio){
                 if(c == null){
                     set.remove(null);
@@ -41,11 +41,14 @@ public class DownTest extends Test
     public void test() {
         try {
             probando_borrado(restart(), new Object[]{"Deborah","Franco","Denisse"});
-            probando_borrado(restart(), new Object[]{ null });
+            probando_borrado(restart(), new Object[]{null});
             probando_borrado(restart(), new Object[]{"Francis","Luis"});
             probando_borrado(elem_1(), new Object[]{"Deborah"});
             probando_borrado(elem_2(), new Object[]{"Deborah"});
+            probando_borrado(elem_2(), new Object[]{"Deborah","Franco"});
             probando_borrado(elem_3(), new Object[]{"Deborah"});
+            probando_borrado(elem_3(), new Object[]{"Deborah","Franco"});
+            probando_borrado(elem_3(), new Object[]{"Deborah","Franco","Tommy"});
         } catch(Exception ex){
             ex.printStackTrace();
         }
